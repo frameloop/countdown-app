9852b344ad2b7687dc80124498055c3344484fe3
Quique <frameloop@gmail.com>
2025-10-19 19:51:08 +0200

    fix: Change title back to 'CounterDown' in landing page

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ const App = () => {
       <div className="max-w-md mx-auto">
                {/* Título */}
                <h1 className="text-3xl font-bold text-center mb-8">
-                 Counter Down
+                 CounterDown
                </h1>
 
         {/* Display odómetro */}
